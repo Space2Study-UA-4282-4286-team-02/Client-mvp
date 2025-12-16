8e6c79d44009f7d7deb19e95a7d0c146d4a97520
Oleh.P <oleg.poruchikov@gmail.com>
2025-12-16 18:25:05 +0200

    feat: add HowItWorksStepBlock component with styles

---
+++ src/components/howItWorksStepBlock/HowItWorksStepBlock.tsx
@@ -0,0 +1,36 @@
+import { useTranslation } from 'react-i18next'
+
+import { Box } from '@mui/material'
+import TitleWithDescription from '../title-with-description/TitleWithDescription'
+
+import { styles } from '../howItWorksStepBlock/HowItWorksStepBlock.styles'
+
+interface HowItWorksStepBlockProps {
+  image: string
+  title: string
+  description: string
+  actionLabel: string
+  cardWidth: number
+}
+
+function HowItWorksStepBlock({
+  image,
+  title,
+  description,
+  cardWidth
+}: HowItWorksStepBlockProps) {
+  const { t } = useTranslation()
+
+  return (
+    <Box sx={{ ...styles.wrapper, maxWidth: cardWidth }}>
+      <Box alt='image' component='img' src={image} sx={styles.img} />
+      <TitleWithDescription
+        description={t(description)}
+        style={styles.titleWithDescription}
+        title={t(title)}
+      />
+    </Box>
+  )
+}
+
+export default HowItWorksStepBlock
