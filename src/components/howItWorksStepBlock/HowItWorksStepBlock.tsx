d5f553bcebf95795665259047eaeed915d151b02
Oleh.P <oleg.poruchikov@gmail.com>
2025-12-17 12:36:31 +0200

    refactor: make image and description props optional in HowItWorksStepBlock component

--- src/components/howItWorksStepBlock/HowItWorksStepBlock.tsx
+++ src/components/howItWorksStepBlock/HowItWorksStepBlock.tsx
@@ -1,15 +1,13 @@
-import { useTranslation } from 'react-i18next'
-
 import { Box } from '@mui/material'
 import TitleWithDescription from '../title-with-description/TitleWithDescription'
 
 import { styles } from '../howItWorksStepBlock/HowItWorksStepBlock.styles'
 
 interface HowItWorksStepBlockProps {
-  image: string
-  alt: string
+  image?: string
+  alt?: string
   title: string
-  description: string
+  description?: string
   cardWidth: number
 }
 
@@ -20,15 +18,13 @@ function HowItWorksStepBlock({
   description,
   cardWidth
 }: HowItWorksStepBlockProps) {
-  const { t } = useTranslation()
-
   return (
     <Box sx={{ ...styles.wrapper, maxWidth: cardWidth }}>
       <Box alt={alt} component='img' src={image} sx={styles.img} />
       <TitleWithDescription
-        description={t(description)}
+        description={description}
         style={styles.titleWithDescription}
-        title={t(title)}
+        title={title}
       />
     </Box>
   )
