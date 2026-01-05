4ed4d9a3f0df40fb4fb7f2b20e0cd8f2ff48d971
Oleh.P <oleg.poruchikov@gmail.com>
2026-01-05 17:08:25 +0200

    feat: enhance LanguageStep
    - Added language selection dropdown using Material-UI components.
    - Integrated translation support for language labels.
    - Improved responsive behavior for image display based on screen size.
    - Updated UserStepsWrapper to use an array for stepLabels based on userRole.

--- src/containers/tutor-home-page/language-step/LanguageStep.jsx
+++ src/containers/tutor-home-page/language-step/LanguageStep.jsx
@@ -1,16 +1,78 @@
-import Box from '@mui/material/Box'
+import { useState } from 'react'
+import { useTranslation } from 'react-i18next'
 
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import useBreakpoints from '~/hooks/use-breakpoints'
+
+import { LanguagesEnum } from '~/types'
 import { styles } from '~/containers/tutor-home-page/language-step/LanguageStep.styles'
 import img from '~/assets/img/tutor-home-page/become-tutor/languages.svg'
 
 const LanguageStep = ({ btnsBox }) => {
+  const [language, setLanguage] = useState('')
+  const { isLaptopAndAbove, isMobile } = useBreakpoints()
+  const { t } = useTranslation()
+
+  const handleChange = (event) => {
+    setLanguage(event.target.value)
+  }
+
+  const languages = Object.values(LanguagesEnum)
+
+  const languagesMenuItems = languages.map((languageValue) => {
+    const translationKey = `common.languages.${languageValue.toLowerCase()}`
+
+    return (
+      <MenuItem key={languageValue} value={languageValue}>
+        {t(translationKey)}
+      </MenuItem>
+    )
+  })
+
   return (
     <Box sx={styles.container}>
-      <Box sx={styles.imgContainer}>
-        <Box component='img' src={img} sx={styles.img} />
-      </Box>
+      {isLaptopAndAbove && (
+        <Box sx={styles.imgContainer}>
+          <Box component='img' src={img} sx={styles.img} />
+        </Box>
+      )}
       <Box sx={styles.rigthBox}>
-        <Box>Language here</Box>
+        <Box>
+          <Typography
+            sx={{
+              marginBottom: '20px',
+              lineHeight: '120%'
+            }}
+            variant={isMobile ? 'body2' : 'subtitle1'}
+          >
+            {t('becomeTutor.languages.title')}
+          </Typography>
+          {isMobile && (
+            <Box sx={styles.imgContainer}>
+              <Box component='img' src={img} sx={styles.img} />
+            </Box>
+          )}
+          <FormControl sx={{ minWidth: '100%' }}>
+            <InputLabel id='your-native-language' sx={{ lineHeight: 1 }}>
+              {t('becomeTutor.languages.autocompleteLabel')}
+            </InputLabel>
+            <Select
+              id='your-native-language'
+              label='Your native language'
+              labelId='your-native-language'
+              onChange={handleChange}
+              value={language}
+            >
+              {languagesMenuItems}
+            </Select>
+          </FormControl>
+        </Box>
+
         {btnsBox}
       </Box>
     </Box>
