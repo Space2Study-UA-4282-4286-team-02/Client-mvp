f903e837354585accb7217826285d61070f48905
Alex <diealex@users.noreply.github.com>
2026-01-26 13:40:39 +0100

    #92-Implement category item card,  #91 Implement list of categories block  (#26)
    * feat[categories]: category item card implemented
    
    * feat[category-item-list]: list of categories implemented
    
    * refactor(categories): deleted http request from List componet
    
    * refactor(categories): moved the list of categories from containers to components
    
    ---------
    
    Co-authored-by: diealex <klochkova.alex91@gmail.com>

---
+++ src/components/category-item-list/CategoryItemList.styles.ts
@@ -0,0 +1,20 @@
+export const styles = {
+  grid: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    alignItems: 'center',
+    gap: '24px'
+  },
+  gridTablet: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(2, 1fr)',
+    alignItems: 'center',
+    gap: '24px'
+  },
+  gridMobile: {
+    display: 'grid',
+    gridTemplateColumns: '1fr',
+    alignItems: 'center',
+    gap: '24px'
+  }
+}
--- src/pages/categories/Categories.tsx
+++ src/pages/categories/Categories.tsx
@@ -1,7 +1,22 @@
 import PageWrapper from '~/components/page-wrapper/PageWrapper'
+import CategoryItemList from '~/components/category-item-list/CategoryItemList'
 
 const Categories = () => {
-  return <PageWrapper>Categories</PageWrapper>
+  return (
+    <PageWrapper>
+      Categories
+      <CategoryItemList
+        items={[
+          {
+            _id: '1',
+            appearance: { color: '#FB8C00', icon: 'math' },
+            name: 'test',
+            totalOffers: { student: 5, tutor: 5 }
+          }
+        ]}
+      />
+    </PageWrapper>
+  )
 }
 
 export default Categories
