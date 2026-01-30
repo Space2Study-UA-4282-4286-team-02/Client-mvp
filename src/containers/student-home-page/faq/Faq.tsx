72e7e4e32102deb2d45cfd67bb598185f17bfde8
lyubomyr <lubomyr.bishko@gmail.com>
2026-01-30 15:21:05 +0200

    feat[faq]: implement accordion component and update modal styles in StudentHome

--- src/containers/student-home-page/faq/Faq.tsx
+++ src/containers/student-home-page/faq/Faq.tsx
@@ -1,15 +1,20 @@
 import { useTranslation } from 'react-i18next'
 
 import Box from '@mui/material/Box'
+import ExpandMoreRoundedIcon from '@mui/icons-material/ExpandMoreRounded'
 
 import TitleWithDescription from '~/components/title-with-description/TitleWithDescription'
 import { studentRoutes } from '~/router/constants/studentRoutes'
 
 import { styles } from '~/containers/student-home-page/faq/Faq.styles'
+import Accordions from '~/components/accordion/Accordions'
+import { useState } from 'react'
+import { accordionItems } from './accordionItems'
+import { TypographyVariantEnum } from '~/types'
 
 const Faq = () => {
   const { t } = useTranslation()
-
+  const [activeItemId, setActiveItemId] = useState<number | null>(null)
   return (
     <Box
       className='section'
@@ -20,6 +25,14 @@ const Faq = () => {
         description={t('studentHomePage.faq.subtitle')}
         style={styles.titleWithDescription}
         title={t('studentHomePage.faq.title')}
+      />
+      <Accordions
+        activeIndex={activeItemId}
+        descriptionVariant={TypographyVariantEnum.Body2}
+        icon={<ExpandMoreRoundedIcon />}
+        items={accordionItems}
+        onChange={(id) => setActiveItemId(id === activeItemId ? null : id)}
+        titleVariant={TypographyVariantEnum.H6}
       />
     </Box>
   )
--- src/pages/student-home/StudentHome.tsx
+++ src/pages/student-home/StudentHome.tsx
@@ -1,12 +1,14 @@
 import { useEffect } from 'react'
-import Container from '@mui/material/Container'
 
 import { useAppSelector } from '~/hooks/use-redux'
 import { useModalContext } from '~/context/modal-context'
+
 import UserStepsWrapper from '~/components/user-steps-wrapper/UserStepsWrapper'
+import PageWrapper from '~/components/page-wrapper/PageWrapper'
 import FindBlock from '~/components/find-block/FindBlock'
 import Faq from '~/containers/student-home-page/faq/Faq'
 
+import { styles } from './StudentHome.styles'
 import { translationKey } from '~/components/find-block/find-tutor-constants'
 
 const StudentHome = () => {
@@ -18,12 +20,7 @@ const StudentHome = () => {
       openModal({
         component: <UserStepsWrapper userRole={userRole} />,
         paperProps: {
-          sx: {
-            maxHeight: { sm: '652px' },
-            height: '100%',
-            maxWidth: '1130px',
-            width: '100%'
-          }
+          sx: styles.modal
         },
         requireConfirmOnClose: true
       })
@@ -31,10 +28,18 @@ const StudentHome = () => {
   }, [openModal, isFirstLogin, userRole])
 
   return (
-    <Container data-testid='studentHome' sx={{ flex: 1 }}>
+    <PageWrapper
+      data-testid='studentHome'
+      sx={{
+        flex: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '80px'
+      }}
+    >
       <FindBlock translationKey={translationKey} />
       <Faq />
-    </Container>
+    </PageWrapper>
   )
 }
 
