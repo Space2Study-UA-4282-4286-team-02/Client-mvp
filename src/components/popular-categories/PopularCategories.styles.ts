cdac411866340e72c8556ad3d8049dac0e299135
lyubomyr <lubomyr.bishko@gmail.com>
2026-02-01 17:15:32 +0200

    feat[popular-categories]: update styles

--- src/components/popular-categories/PopularCategories.styles.ts
+++ src/components/popular-categories/PopularCategories.styles.ts
@@ -1,17 +1,19 @@
 export const styles = {
   container: {
-    flexDirection: 'column'
+    flexDirection: 'column',
+    pt: { xs: '0px', sm: '14px' },
+    pb: { xs: '0px', sm: '33px' }
   },
   titleWithDescription: {
     wrapper: {
       textAlign: 'center',
-      mb: '32px'
+      mb: { xs: '16px', sm: '32px' }
     },
     title: {
-      typography: { xs: 'h4' }
+      typography: { xs: 'h5', sm: 'h4' }
     },
     description: {
-      typography: { xs: 'subtitle1' }
+      typography: { xs: 'body2', sm: 'body1' }
     }
   }
 }
