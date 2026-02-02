61730002bb17b384260cb62e9dc9616e91cbb755
lyubomyr <80955821+lyubomyrb@users.noreply.github.com>
2026-02-02 20:35:58 +0200

    #136-[Home page] Implement popular categories block (#35)
    * feat[popular-categories]: add PopularCategories component and styles, include category icon helper
    
    * feat[popular-categories]: make PopularCategories component universal for student and tutor roles
    
    * feat[popular-categories]: update styles
    
    * feat(category-icons): expand CategoryIconType and update icon mapping

---
+++ src/components/popular-categories/PopularCategories.styles.ts
@@ -0,0 +1,19 @@
+export const styles = {
+  container: {
+    flexDirection: 'column',
+    pt: { xs: '0px', sm: '14px' },
+    pb: { xs: '0px', sm: '33px' }
+  },
+  titleWithDescription: {
+    wrapper: {
+      textAlign: 'center',
+      mb: { xs: '16px', sm: '32px' }
+    },
+    title: {
+      typography: { xs: 'h5', sm: 'h4' }
+    },
+    description: {
+      typography: { xs: 'body2', sm: 'body1' }
+    }
+  }
+}
--- src/pages/student-home/StudentHome.tsx
+++ src/pages/student-home/StudentHome.tsx
@@ -6,10 +6,12 @@ import { useModalContext } from '~/context/modal-context'
 import UserStepsWrapper from '~/components/user-steps-wrapper/UserStepsWrapper'
 import PageWrapper from '~/components/page-wrapper/PageWrapper'
 import FindBlock from '~/components/find-block/FindBlock'
+import PopularCategories from '~/components/popular-categories/PopularCategories'
 import Faq from '~/containers/student-home-page/faq/Faq'
 
 import { styles } from './StudentHome.styles'
 import { translationKey } from '~/components/find-block/find-tutor-constants'
+import { translationKey as popularCategoriesKey } from '~/components/popular-categories/popular-student-constants'
 
 const StudentHome = () => {
   const { openModal } = useModalContext()
@@ -30,6 +32,7 @@ const StudentHome = () => {
   return (
     <PageWrapper data-testid='studentHome' sx={styles.pageWrapper}>
       <FindBlock translationKey={translationKey} />
+      <PopularCategories translationKey={popularCategoriesKey} />
       <Faq />
     </PageWrapper>
   )
